fd7b77f9249771c6b204938bde26d29bd3fd4e85
Yulia <y.ekler1@gmail.com>
2022-02-08 12:27:38 +0200

    second module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,14 @@
+
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for (let key in salaries) {
+    let salary = typeof (salaries[key]) === 'number' && Number.isFinite(salaries[key]);
+
+    if (salary) {
+      sum += salaries[key];
+    }
+  }
+
+  return sum;
 }
