3b427ed2db0c0968e4b85ce7f6be2464949ea5a1
Yulia <y.ekler1@gmail.com>
2022-02-10 12:55:22 +0200

    1, 2, 3 task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,11 @@
+let arr = [5, 3, 8, 1];
+
+
 function filterRange(arr, a, b) {
-  // ваш код...
+  return arr.filter(i => (a <= i && i <= b ))
 }
+
+let filtered = filterRange(arr, 1, 4);
+
+console.log( filtered ); // [3,1] (совпадающие значения)
+console.log( arr );
