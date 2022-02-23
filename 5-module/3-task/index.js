e94c08e92c10e091df0529732ee93d1440df5aa2
Yulia <y.ekler1@gmail.com>
2022-02-23 17:08:34 +0200

    Carousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,39 @@
 function initCarousel() {
-  // ваш код...
+   let carousel = document.querySelector('.carousel__inner'),
+       carouselRight = document.querySelector('.carousel__arrow_right'),
+       carouselLeft = document.querySelector('.carousel__arrow_left'),
+       carouselInnerWidth = document.querySelector(".carousel__inner").offsetWidth,
+       countSlide = document.querySelectorAll('.carousel__slide').length;
+
+       let count = 0;
+
+       setArrow(count, carouselRight, carouselLeft, countSlide);
+
+     carouselRight.addEventListener('click' , () => {
+        count++;
+        carousel.style.transform = `translateX(-${count * carouselInnerWidth}px)`;
+        setArrow(count, carouselRight, carouselLeft, countSlide);
+
+     });
+
+     carouselLeft.addEventListener('click' , () => {
+        count--;
+        carousel.style.transform = `translateX(-${count * carouselInnerWidth}px)`;
+        setArrow(count, carouselRight, carouselLeft, countSlide);
+     });
+
+     function setArrow(count, carouselRight, carouselLeft, countSlide) {
+       if(count === 0) {
+        carouselLeft.style.display = 'none';
+       } else {
+        carouselLeft.style.display = '';
+       }
+
+       if(count >= countSlide - 1) {
+        carouselRight.style.display = 'none';
+       } else {
+        carouselRight.style.display = '';
+       }
+     }
+
 }
